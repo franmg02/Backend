cf663297086a885f944530753ac6702ff885aba0
franmg02 <fjmolinag@uqvirtual.edu.co>
2019-06-14 00:09:00 -0500

    Prueba heroku

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
 $arreglo = $_POST["arreglo"];
 
-$respuesta = trim(shell_exec("C:/\"Program Files (x86)\"/\"Microsoft Visual Studio\"/Shared/Python36_64/python NeuralNetwork.py ".($arreglo)." 2>&1;"));
+$respuesta = trim(shell_exec("python NeuralNetwork.py ".($arreglo)." 2>&1;"));
 
 echo '{ "salida": "'.$respuesta.'" }';
 
